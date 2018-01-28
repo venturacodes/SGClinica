0d065d214fadafa6357816562589c60fb37a065a
Arthur Alves Venturin <arthur.alves.venturin@gmail.com>
2018-01-28 17:46:45 -0200

    Organizado arquivo de api, modificado api clinic para clinics.;

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -11,27 +12,31 @@ use Illuminate\Http\Request;
 | is assigned the "Api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('Api')->get('/users', 'api\UserController@index');
-Route::middleware('Api')->get('/users/email/{email}/password/{password}', 'api\UserController@show');
-Route::middleware('Api')->post('/users', 'api\UserController@store');
-Route::middleware('Api')->post('/users/{id}','api\UserController@update')->where(['id' => '[0-9]+']);
-Route::middleware('Api')->delete('/users/{id}','api\UserController@delete')->where(['id' => '[0-9]+']);
-
-Route::middleware('Api')->get('/clinic', 'api\ClinicController@index');
-Route::middleware('Api')->post('/clinic/{id}', 'api\ClinicController@update');
-Route::middleware('Api')->delete('/clinic{id}', 'api\ClinicController@delete');
-
-Route::middleware('Api')->get('/clients', 'api\ClientController@index');
-Route::middleware('Api')->post('/clients', 'api\ClientController@create');
-Route::middleware('Api')->post('/clients/{id}', 'api\ClientController@update')->where(['id' => '[0-9]+']);
-Route::middleware('Api')->delete('/clients/{id}', 'api\ClientController@delete')->where(['id' => '[0-9]+']);
-
 Route::middleware('Api')->get('/collaborators', 'api\CollaboratorController@index');
 Route::middleware('Api')->get('/jobs', 'api\JobController@index');
 
-Route::middleware('Api')->get('/appointments/clinic/{clinic_id}', 'api\AppointmentController@showByClinicId')->where(['clinic_id' => '[0-9]+']);
-Route::middleware('Api')->get('/appointments/client/{client_id}', 'api\AppointmentController@showByClientId')->where(['client_id' => '[0-9]+']);
-Route::middleware('Api')->get('/appointments/collaborator/{collaborator_id}', 'api\AppointmentController@showByCollaboratorId')->where(['collaborator_id' => '[0-9]+']);
+Route::prefix('users', function () {
+    Route::middleware('Api')->get('/', 'api\UserController@index');
+    Route::middleware('Api')->get('/email/{email}/password/{password}', 'api\UserController@show');
+    Route::middleware('Api')->post('/', 'api\UserController@store');
+    Route::middleware('Api')->post('/{id}', 'api\UserController@update')->where(['id' => '[0-9]+']);
+    Route::middleware('Api')->delete('/{id}', 'api\UserController@delete')->where(['id' => '[0-9]+']);
+});
 
-Route::middleware('Api')->post('/appointments', 'api\AppointmentController@create');
+Route::prefix('clinics', function () {
+    Route::middleware('Api')->get('/', 'api\ClinicController@index');
+    Route::middleware('Api')->post('/{id}', 'api\ClinicController@update');
+    Route::middleware('Api')->delete('/{id}', 'api\ClinicController@delete');
+});
+Route::prefix('clients', function () {
+    Route::middleware('Api')->get('/', 'api\ClientController@index');
+    Route::middleware('Api')->post('/', 'api\ClientController@create');
+    Route::middleware('Api')->post('/{id}', 'api\ClientController@update')->where(['id' => '[0-9]+']);
+    Route::middleware('Api')->delete('/{id}', 'api\ClientController@delete')->where(['id' => '[0-9]+']);
+});
+Route::prefix('appointments', function () {
+    Route::middleware('Api')->get('/clinic/{clinic_id}', 'api\AppointmentController@showByClinicId')->where(['clinic_id' => '[0-9]+']);
+    Route::middleware('Api')->get('/client/{client_id}', 'api\AppointmentController@showByClientId')->where(['client_id' => '[0-9]+']);
+    Route::middleware('Api')->get('/collaborator/{collaborator_id}', 'api\AppointmentController@showByCollaboratorId')->where(['collaborator_id' => '[0-9]+']);
+    Route::middleware('Api')->post('/', 'api\AppointmentController@create');
+});
