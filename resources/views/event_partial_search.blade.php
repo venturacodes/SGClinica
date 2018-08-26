c7204522dad55631e50e08c22191b9ee17898888
ArthurVenturin <arthur.alves.venturin@gmail.com>
2018-08-26 08:58:08 -0300

    Modificado select de pesquisa de médico, estava trazendo o nome do cliente, alterado para nome do collaborator.

--- resources/views/event_partial_search.blade.php
+++ resources/views/event_partial_search.blade.php
@@ -4,9 +4,9 @@
         <label for="title">Agenda do médico</label>
         <select class="form-control" id="client" name="client" style="width: 100%" required>
         <option value="0">Selecione um médico</option>
-        @foreach($data['clients'] as $clientes)
-            @foreach($clientes as $cliente)
-                <option value="{{$cliente->id}}">{{$cliente->name}}</option>
+        @foreach($data['collaborators'] as $collaborators)
+            @foreach($collaborators as $collaborator)
+                <option value="{{$collaborator->id}}">{{$collaborator->name}}</option>
              @endforeach
         @endforeach
         </select>
