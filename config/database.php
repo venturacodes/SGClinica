e662992be64693a780e05b570a950f7e69293739
ArthurVenturin <arthur.alves.venturin@gmail.com>
2018-08-26 15:57:30 -0300

    Heroku modificado arquivo database.php para espelhar configuração da base de dados do heroku.

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -12,6 +19,7 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
+
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
@@ -41,11 +49,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'port' => $port,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
