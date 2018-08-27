0c0aff3f3f3c13187aba7489ece2659fe86522c6
ArthurVenturin <arthur.alves.venturin@gmail.com>
2018-08-27 10:04:11 -0300

    Heroku

--- resources/views/auth/authWidget.blade.php
+++ resources/views/auth/authWidget.blade.php
@@ -11,37 +11,37 @@
                     <li><a href="{{ route('login') }}">Entrar</a></li>
                     <li><a href="{{ route('register') }}">Registrar-se</a></li>
                 @else
-                <li >
+        <li style="background-color: #448DBA;">
             <a href="{{ route('appointment.show') }}">
                 <i class="glyphicon glyphicon-calendar"></i> <span>Agenda</span>
             </a>
         </li>
-        <li>
+        <li style="background-color: #448DBA;">
             <a href="{{ route('collaborator.index') }}">
                 <i class="glyphicon glyphicon-user"></i> <span>Médicos</span>
             </a>
-        </li>
-        <li>
+        </li >
+        <li style="background-color: #448DBA;">
             <a href="{{ route('clinic.index') }}">
                 <i class="glyphicon glyphicon-briefcase"></i> <span>Clínicas</span>
             </a>
-        </li>
-        <li>
+        </li >
+        <li style="background-color: #448DBA;">
             <a  href="{{ route('client.index') }}">
                 <i class="glyphicon glyphicon-heart"></i> <span>Pacientes</span>
             </a>
         </li>
-        <li>
+        <li style="background-color: #448DBA;">
             <a  href="{{ route('medicine.index') }}">
                 <i class="glyphicon glyphicon-asterisk"></i> <span>Medicamentos</span>
             </a>
         </li>
-        <li>
+        <li style="background-color: #448DBA;">
             <a  href="{{ route('receipt.index') }}">
                 <i class="glyphicon glyphicon-asterisk"></i> <span>Receitas</span>
             </a>
         </li>
-                    <li class="dropdown">
+                    <li class="dropdown" style="background-color: #448DBA;">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             {{ Auth::user()->email }} <span class="caret"></span>
                         </a>
