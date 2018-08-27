c0d71770499876ede985af62e283cccf21e429f7
ArthurVenturin <arthur.alves.venturin@gmail.com>
2018-08-27 20:09:14 -0300

    Mudado database.php.

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 
 // só usa no heroku
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
 
 return [
 
@@ -49,17 +49,17 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             #usa local
-            // 'host' => env('DB_HOST', '127.0.0.1'),
-            // 'port' => env('DB_PORT', '3306'),
-            // 'database' => env('DB_DATABASE', 'forge'),
-            // 'username' => env('DB_USERNAME', 'forge'),
-            // 'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             #usa no heroku
-            'host' => $host,
-            'port' => $port,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            // 'host' => $host,
+            // 'port' => $port,
+            // 'database' => $database,
+            // 'username' => $username,
+            // 'password' => $password,
             #
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
