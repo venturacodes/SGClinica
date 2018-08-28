82cf493da7cd6b0fc26d13a724f39081fde0ef06
ArthurVenturin <arthur.alves.venturin@gmail.com>
2018-08-28 10:33:56 -0300

    Heroku

--- app/Http/Controllers/Web/AppointmentController.php
+++ app/Http/Controllers/Web/AppointmentController.php
@@ -34,18 +34,7 @@ class AppointmentController extends Controller
      */
     public function create(Request $request)
     {
-        $data = $request->all();
-        $appointment = new Appointment();
-        $data['start_time'] = new Carbon($request->start_time);
-        $data['end_time'] = new Carbon($request->start_time);
-        $data['end_time']->addMinutes(Appointment::DEFAULT_DURATION);
-        $already_booked = $appointment->checkIfAlreadyBooked($data['start_time'], $data['end_time'],  $data['clinic_id']);
-        if ($already_booked) {
-            return new JsonResponse(['message' => 'Não foi possivel agendar consulta, horário não disponível.']);
-        }
-        $appointment->prepare($data);
-        $appointment->save();
-        return new JsonResponse($appointment);
+        
     }
     /**
      * Show appointments by it's client_id.
