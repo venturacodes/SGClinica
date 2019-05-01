e4b262f365fb3bf80fd9f997ad63ad3bf5dd75d8
arthurvento <arthur.alves.venturin@gmail.com>
2019-05-01 18:09:49 -0300

    ultimo commmit antes da atualização para o 5.8

--- resources/views/medicine/form.blade.php
+++ resources/views/medicine/form.blade.php
@@ -4,15 +4,16 @@
 @endsection
 @section('form_title','Editar medicamento')
 @section('form_content')
-    <form method="POST" action="{{$medicine->id ? route('medicine.update', $medicine->id) : route('medicine.store') }}">
+    <form method="POST" action="{{ isset($medicine)? route('medicine.update', $medicine->id) : route('medicine.store') }}">
+        
         {{ csrf_field() }}
-        @method('PUT')
+        
         <label for="name">Nome genérico</label>
-        <input type="text" name="generic_name" class="form-control" value="{{$medicine->generic_name ? $medicine->generic_name : ''}}">
+        <input type="text" name="generic_name" class="form-control" value="{{ isset($medicine) ? $medicine->generic_name : ''}}">
         <label for="phone">Nome de fábrica</label>
-        <input type="text" name="manufacturer_name"  class="form-control" value="{{$medicine->manufacturer_name ? $medicine->manufacturer_name : ''}}">
+        <input type="text" name="manufacturer_name"  class="form-control" value="{{ isset($medicine) ? $medicine->manufacturer_name : ''}}">
         <label for="phone">Fábrica</label>
-        <input type="text" name="manufacturer"  class="form-control" value="{{$medicine->manufacturer ? $medicine->manufacturer : ''}}">
+        <input type="text" name="manufacturer"  class="form-control" value="{{isset($medicine) ? $medicine->manufacturer : ''}}">
         <br />
         <input type="submit" value="salvar" class="btn btn-block btn-primary">
     </form>
